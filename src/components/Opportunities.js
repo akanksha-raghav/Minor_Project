8ca57a81d403c68d526c64fe0d1b648148216e90
Akanksha Raghav <bitturaghav919@gmail.com>
2020-11-09 15:16:12 +0530

    Update Opportunities.js

--- src/components/Opportunities.js
+++ src/components/Opportunities.js
@@ -12,7 +12,7 @@ const Opportunities = () => {
     >
       <div className="container has-text-centered">
         <h1 className="title">
-          Are you looking for some Opportunities? &#129320;
+          Are you looking for some Opportunities? &#128519;
         </h1>
         <h2 className="subtitle has-text-weight-semibold">
           Here you can check the different Opportunities and grab them!
