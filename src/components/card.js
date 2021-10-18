2c6962aeff25a16c094d188f2d3edf01eb30a97e
irsayvid <dsdarimisetti@gmail.com>
2021-10-18 12:20:48 +0530

    make card size uniform

--- src/components/card.js
+++ src/components/card.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React from 'react'
 
 const card = (elem) => {
   return (
     <div
       className="has-text-weight-semibold"
       style={{
-        borderRadius: "30px",
-        backgroundColor: "white",
-        width: "220px",
-        padding: "10px",
+        borderRadius: '30px',
+        backgroundColor: 'white',
+        width: '220px',
+        minHeight: '190px',
+        padding: '10px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
       }}
     >
       <div className="has-text-centered">
@@ -19,20 +23,20 @@ const card = (elem) => {
           width="50px"
         />
         <p className="is-size-5">
-          {" "}
+          {' '}
           <a
             href={elem.data.elem.link}
             target="_blank"
             rel="noopener noreferrer"
           >
-            {" "}
-            {elem.data.elem.name}{" "}
+            {' '}
+            {elem.data.elem.name}{' '}
           </a>
         </p>
         <br />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default card;
+export default card
